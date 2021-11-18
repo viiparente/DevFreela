28fb129ac11115e669b59969ec7cdae286c6199d
Vinicius Parente <vd.parente@hotmail.com>
2021-11-17 21:50:11 -0300

    Application Settings

--- DevFreela/DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela/DevFreela.API/Controllers/ProjectsController.cs
@@ -1,11 +1,17 @@
 ﻿using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/projects")]
     public class ProjectsController : ControllerBase
     {
+        private readonly OpeningTimeOption _option;
+        public ProjectsController(IOptions<OpeningTimeOption> option)
+        {
+            _option = option.Value;
+        }
         // Aqui está utilizando a requisiçao via  parâmetro /api/projects e a ?query= o nome que deseja
         // api/projects?query=net core
         [HttpGet]
