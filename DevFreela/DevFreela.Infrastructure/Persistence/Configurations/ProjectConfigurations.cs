9ba0f68b3e59d32b95cf6b1c08b2a0a5bc6fb4a9
Vinicius Parente <vd.parente@hotmail.com>
2021-11-30 21:00:46 -0300

    Refatorando as configurações / Entidades

---
+++ DevFreela/DevFreela.Infrastructure/Persistence/Configurations/ProjectConfigurations.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Configurations
+{
+    public class ProjectConfigurations : IEntityTypeConfiguration<Project>
+    {
+        public void Configure(EntityTypeBuilder<Project> builder)
+        {
+            builder
+                .HasKey(p => p.Id);
+
+            //um freelancer tem muitos projetos e tem uma chave estrangeria.
+            builder
+                .HasOne(p => p.Freelancer)
+                .WithMany(f => f.FreelanceProjects)
+                .HasForeignKey(p => p.IdFreelancer)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder
+               .HasOne(p => p.Cliente)
+               .WithMany(f => f.OwnedProjects)
+               .HasForeignKey(p => p.IdClient)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
--- DevFreela/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela/DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DevFreela.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace DevFreela.Infrastructure.Persistence
 {
@@ -20,50 +21,15 @@ namespace DevFreela.Infrastructure.Persistence
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Project>()
-                .HasKey(p => p.Id);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
-            //um freelancer tem muitos projetos e tem uma chave estrangeria.
-            modelBuilder.Entity<Project>()
-                .HasOne(p => p.Freelancer)
-                .WithMany(f => f.FreelanceProjects)
-                .HasForeignKey(p => p.IdFreelancer)
-                .OnDelete(DeleteBehavior.Restrict);
+            
 
-            modelBuilder.Entity<Project>()
-               .HasOne(p => p.Cliente)
-               .WithMany(f => f.OwnedProjects)
-               .HasForeignKey(p => p.IdClient)
-               .OnDelete(DeleteBehavior.Restrict);
+            
 
-            modelBuilder.Entity<ProjectComment>()
-               .HasKey(p => p.Id);
+          
 
-            modelBuilder.Entity<ProjectComment>()
-                .HasOne(p => p.Project)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(p => p.IdProject);
-
-            modelBuilder.Entity<ProjectComment>()
-                .HasOne(p => p.User)
-                .WithMany(p => p.Comments)
-                .HasForeignKey(p => p.IdUser);
-
-            modelBuilder.Entity<Skill>()
-               .HasKey(p => p.Id);
-
-            modelBuilder.Entity<User>()
-               .HasKey(p => p.Id);
-
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Skills)
-                .WithOne()
-                .HasForeignKey(p => p.IdSkill)
-                .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<UserSkill>()
-               .HasKey(p => p.Id);
+            
         }
     }
 
