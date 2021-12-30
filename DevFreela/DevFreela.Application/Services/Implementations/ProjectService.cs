97dcc06106baae45493cf558e1069ed555ad17ef
Vinicius Parente <vd.parente@hotmail.com>
2021-12-29 21:49:35 -0300

    Refatorando com Dapper

--- DevFreela/DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela/DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,19 +1,26 @@
-﻿using DevFreela.Application.InputModels;
+﻿using Dapper;
+using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Application.Services.Implementations
 {
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
-        public ProjectService(DevFreelaDbContext dbContext)
+        private readonly string _connectionString;
+        public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
+
         }
+
         public int Create(NewProjectInputModel inputModel)
         {
             var project = new Project(inputModel.Title, inputModel.Description,inputModel.IdClient,inputModel.IdFreelancer, inputModel.TotalCost);
@@ -96,7 +103,16 @@ namespace DevFreela.Application.Services.Implementations
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
 
             project.Start();
-            _dbContext.SaveChanges();
+            //_dbContext.SaveChanges();
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "UPDATE Projects SET Status = @status, StartedAt = @startedat WHERE Id = @id";
+
+                sqlConnection.Execute(script, new {status = project.Status, startedat = project.StartedAt, id});
+                // return sqlConnection.Query<SkillViewModel>(script).ToList();
+            }
         }
          
         public void Update(UpdateProjectInputModel inputModel)
--- DevFreela/DevFreela.Application/Services/Implementations/SkillService.cs
+++ DevFreela/DevFreela.Application/Services/Implementations/SkillService.cs
@@ -1,6 +1,9 @@
-﻿using DevFreela.Application.Services.Interfaces;
+﻿using Dapper;
+using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Application.Services.Implementations
 {
@@ -8,19 +11,30 @@ namespace DevFreela.Application.Services.Implementations
     public class SkillService : ISkillService
     {
         private readonly DevFreelaDbContext _dbContext;
-        public SkillService(DevFreelaDbContext dbContext)
+
+        private readonly string _connectionString;
+        public SkillService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
         public List<SkillViewModel> GetAll()
         {
-            var skills = _dbContext.Skills;
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
 
-            var skillViewModel = skills
-                .Select(x => new SkillViewModel(x.Id, x.Description))
-                .ToList();
+                var script = "SELECT Id, Description FROM Skills";
 
-            return skillViewModel;
+                return sqlConnection.Query<SkillViewModel>(script).ToList();
+            }
+            //var skills = _dbContext.Skills;
+
+            //var skillViewModel = skills
+            //    .Select(x => new SkillViewModel(x.Id, x.Description))
+            //    .ToList();
+
+            //return skillViewModel;
         }
     }
 }
