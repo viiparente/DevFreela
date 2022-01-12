9e6abf7c8055bab671e7aa58e8cb03473367aa03
Vinicius Parente <vd.parente@hotmail.com>
2022-01-11 22:13:34 -0300

    Gerando Migration com dados de Login

--- DevFreela/DevFreela.API/Startup.cs
+++ DevFreela/DevFreela.API/Startup.cs
@@ -2,6 +2,8 @@
 using DevFreela.Application.Commands.CreateProject;
 using DevFreela.Application.Validators;
 using DevFreela.Core.Repositories;
+using DevFreela.Core.Services;
+using DevFreela.Infrastructure.Auth;
 using DevFreela.Infrastructure.Persistence;
 using DevFreela.Infrastructure.Persistence.Repositories;
 using FluentValidation.AspNetCore;
@@ -49,7 +51,7 @@ namespace DevFreela.API
             services.AddScoped<IProjectRepository, ProjectRepository>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ISkillRepository, SkillRepository>();
-
+            services.AddScoped<IAuthService, AuthService>();
 
             services.AddControllers(options => options.Filters.Add(typeof(ValidationFilter)))
                 .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateUserCommandValidator>());  
